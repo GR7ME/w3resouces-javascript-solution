bd28f758b8fbf2f1100d76456ef3c7536b5bd303
GR7ME <lsinjali40@gmail.com>
2022-06-23 17:16:43 +0545

    change in solution of problem 1

--- ES6problem1.js
+++ ES6problem1.js
@@ -1,7 +1,22 @@
+//For object to make it iterable
+//Suppose an object 
+/*
+obj ={
+name:"lalit",
+age:24
+}
+
+Object.keys(obj) -> gives an array with keys of object -> [name,age] 
+
+*/
+
+
+//My solution
+//solution1
 const obj1 ={
     name:"lalit",
     age:24,
-    friend:"kamal"
+    friend:"human"
 }
 
 const obj2 ={
@@ -9,17 +24,31 @@ const obj2 ={
     age:24,
     friend:"kamal"
 }
-let len = Object.keys(obj1).length
+// let len = Object.keys(obj1).length
 let isSame = true
 
-for (let i=0;i<len;i++){
-    if (obj1[Object.keys(obj1)[i]] == obj2[Object.keys(obj2)[i]]){
+// for (let i=0;i<len;i++){
+//     if (obj1[Object.keys(obj1)[i]] == obj2[Object.keys(obj2)[i]]){
+//         isSame = true    
+//     }
+//     else{
+//         isSame = false
+//     }
+// }
+
+
+// console.log(isSame)
+
+//solution2
+//Optimal solution
+
+Object.keys(obj1).forEach(function(key){
+    if (obj1[key] == obj2[key]){
         isSame = true    
     }
     else{
         isSame = false
     }
-}
-
+})
 
 console.log(isSame)
